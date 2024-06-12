01132753b619013acb9b86a2639bc72a14e64156
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-12 17:11:18 +0300

    add the function handle_create_recipes

--- src/bin/recipes_iter_0.rs
+++ src/bin/recipes_iter_0.rs
@@ -96,6 +96,34 @@ async fn read_local_recipes() -> Result <Recipes, Box<dyn Error>> {
   Ok(result)
 }
 
+async fn create_new_story(name: &str, ingredients: &str, instructions: &str) -> Result<(), Box<dyn Error>> {
+  let mut local_recipes = read_local_recipes().await?;
+  let new_id = match local_recipes.iter().max_by_key(|r| r.id) {
+    Some(v) => v.id + 1,
+    None => 0,
+  };
+  local_recipes.push(Recipe {
+    id: new_id,
+    name: name.to_owned(),
+    ingredients: ingredients.to_owned(),
+    instructions: instructions.to_owned(),
+    public: false,
+  });
+  write_local_recipes(&local_recipes).await?;
+
+  info!("Created story:");
+  info!("Name: {}", name);
+  info!("Ingredients: {}", ingredients);
+  info!("Instructions:: {}", instructions);
+
+  Ok(())
+} 
+
+async fn write_local_recipes(recipes: &Recipes) -> Result<(), Box<dyn Error>> {
+  let json = serde_json::to_string(&recipes)?;
+  fs::write(STORAGE_FILE_PATH, &json).await?;
+  Ok(())
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   let _ = tracing_subscriber::fmt()
@@ -186,7 +214,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             EventType::Input(line) => match line.as_str() {
              "ls p" => handle_list_peers(&mut swarm).await,
               cmd if cmd.starts_with("ls_r") => handle_list_recipes(cmd, &mut swarm).await,
-//              cmd if cmd.starts_with("create r") => handle_create_recipe(cmd).await,
+              cmd if cmd.starts_with("create r") => handle_create_recipes(cmd).await,
 //              cmd if cmd.starts_with("publish r") => handle_publish_recipe(cmd).await,
               _ => error!("unknown command"),
             }
@@ -252,3 +280,18 @@ async fn handle_list_recipes(cmd: &str, swarm: &mut Swarm<RecipeBehaviour>) {
   }
 }
 
+async fn handle_create_recipes(cmd: &str) {
+  if let Some(rest) = cmd.strip_prefix("create s") {
+    let elements: Vec<&str> = rest.split('|').collect();
+    if elements.len() < 3 {
+      info!("too few arguments: Format name|header|body");
+    } else {
+      let name = elements.get(0).expect("name is there");
+      let header = elements.get(1).expect("header is there");
+      let body = elements.get(2).expect("body is there");
+      if let Err(e) = create_new_story(name, header, body).await {
+        error!("error creating story: {}", e);
+      };
+    }
+  }
+}
